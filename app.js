830f40dda0e3f94ce79e5c699e6637bbede68e55
hsinyi8997 <hsinyi8997@gmail.com>
2022-01-11 11:14:28 +0800

    feat: add main.hbs and index.hbs

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
+const moment = require('moment')
 const router = require('./routers')
 const app = express()
 const PORT = 3000
@@ -12,6 +13,7 @@ app.engine('hbs', exphbs.engine({ defaultLayout:'main', extname:'.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+app.use(express.static('public'))
 
 app.use(router)
 
